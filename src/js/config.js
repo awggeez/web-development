758e52367f5ecdfc2e0f84284d046ba10ed9b748
AbMa10 <moabiev@edu.hse.ru>
2023-07-11 12:18:32 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cea101bf-c555-4315-ae0f-7f4b340da10b';
+export const MODAL_CLOSE_SEC = 2.5;
